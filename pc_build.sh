e9a1feb66f250355f8fe69cbfd4cbc6ec066ae1c
nexus <122535095+nexuss2@users.noreply.github.com>
2026-05-04 16:45:24 +0000

    Build PC env shim in runner

--- pc_build.sh
+++ pc_build.sh
@@ -10,8 +10,20 @@ cc -DPLATFORM_PC -c src/platform/pc/memory_pc.c -o build/pc/memory_pc.o
 cc -DPLATFORM_PC -c src/platform/pc/dvdmgr_pc.c -o build/pc/dvdmgr_pc.o
 cc -DPLATFORM_PC -c src/platform/pc/arcdrv_pc.c -o build/pc/arcdrv_pc.o
 cc -DPLATFORM_PC -c src/platform/pc/fadedrv_pc.c -o build/pc/fadedrv_pc.o
+cc -DPLATFORM_PC -c src/platform/pc/envdrv_pc.c -o build/pc/envdrv_pc.o
 
-cc -DPLATFORM_PC pc_main.c build/pc/file.o build/pc/input.o build/pc/time.o build/pc/memory_pc.o build/pc/dvdmgr_pc.o build/pc/arcdrv_pc.o build/pc/fadedrv_pc.o -o build/pc/ttyd_pc
+cc -DPLATFORM_PC \
+  pc_main.c \
+  build/pc/file.o \
+  build/pc/input.o \
+  build/pc/time.o \
+  build/pc/memory_pc.o \
+  build/pc/dvdmgr_pc.o \
+  build/pc/arcdrv_pc.o \
+  build/pc/fadedrv_pc.o \
+  build/pc/envdrv_pc.o \
+  -o build/pc/ttyd_pc
+
 cc -DPLATFORM_PC pc_asset_test.c build/pc/file.o -o build/pc/asset_test
 cc -DPLATFORM_PC pc_arcdrv_test.c build/pc/file.o build/pc/arcdrv_pc.o -o build/pc/arcdrv_test
 
