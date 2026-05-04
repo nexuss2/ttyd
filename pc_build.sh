f8603e87b797ec9efdb0a37e8c7b97e86346d886
nexus <122535095+nexuss2@users.noreply.github.com>
2026-05-04 16:37:11 +0000

    Build PC arcOpen shim in smoke runner

--- pc_build.sh
+++ pc_build.sh
@@ -6,16 +6,14 @@ mkdir -p build/pc
 cc -DPLATFORM_PC -c src/platform/pc/file.c -o build/pc/file.o
 cc -DPLATFORM_PC -c src/platform/pc/input.c -o build/pc/input.o
 cc -DPLATFORM_PC -c src/platform/pc/time.c -o build/pc/time.o
-cc -DPLATFORM_PC -c src/platform/pc/dvdmgr_pc.c -o build/pc/dvdmgr_pc.o
 cc -DPLATFORM_PC -c src/platform/pc/memory_pc.c -o build/pc/memory_pc.o
+cc -DPLATFORM_PC -c src/platform/pc/dvdmgr_pc.c -o build/pc/dvdmgr_pc.o
+cc -DPLATFORM_PC -c src/platform/pc/arcdrv_pc.c -o build/pc/arcdrv_pc.o
 
-cc -DPLATFORM_PC \
-  pc_main.c \
-  build/pc/file.o \
-  build/pc/input.o \
-  build/pc/time.o \
-  build/pc/dvdmgr_pc.o \
-  build/pc/memory_pc.o \
-  -o build/pc/ttyd_pc
+cc -DPLATFORM_PC pc_main.c build/pc/file.o build/pc/input.o build/pc/time.o build/pc/memory_pc.o build/pc/dvdmgr_pc.o build/pc/arcdrv_pc.o -o build/pc/ttyd_pc
+cc -DPLATFORM_PC pc_asset_test.c build/pc/file.o -o build/pc/asset_test
+cc -DPLATFORM_PC pc_arcdrv_test.c build/pc/file.o build/pc/arcdrv_pc.o -o build/pc/arcdrv_test
 
 ./build/pc/ttyd_pc
+./build/pc/asset_test
+./build/pc/arcdrv_test
