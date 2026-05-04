f5d49602f7442ce1098301f43af66134bcae64a5
nexus <122535095+nexuss2@users.noreply.github.com>
2026-05-04 13:45:06 +0000

    Implement basic PC input test state

--- src/platform/pc/input.c
+++ src/platform/pc/input.c
@@ -1,10 +1,21 @@
 #include <dolphin/pad.h>
 
+static u16 s_buttons;
+static s8 s_stick_x;
+static s8 s_stick_y;
+
 void PlatformInputInit(void) {
+    s_buttons = 0;
+    s_stick_x = 0;
+    s_stick_y = 0;
 }
 
 void PlatformInputRead(PADStatus* pads) {
     int i;
+
+    s_buttons = PAD_BUTTON_A;
+    s_stick_x = 0;
+    s_stick_y = 80;
 
     for (i = 0; i < 4; i++) {
         pads[i].button = 0;
@@ -18,6 +29,10 @@ void PlatformInputRead(PADStatus* pads) {
         pads[i].analogB = 0;
         pads[i].err = 0;
     }
+
+    pads[0].button = s_buttons;
+    pads[0].stickX = s_stick_x;
+    pads[0].stickY = s_stick_y;
 }
 
 void PlatformInputReset(unsigned int resetMask) {
