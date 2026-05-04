fd99ae6ce22e682e8d21d01868fa355e259fe6b8
nexus <122535095+nexuss2@users.noreply.github.com>
2026-05-04 16:24:36 +0000

    Build PC DVD shim in smoke runner

--- pc_build.sh
+++ pc_build.sh
@@ -6,3 +6,14 @@ mkdir -p build/pc
 cc -DPLATFORM_PC -c src/platform/pc/file.c -o build/pc/file.o
 cc -DPLATFORM_PC -c src/platform/pc/input.c -o build/pc/input.o
 cc -DPLATFORM_PC -c src/platform/pc/time.c -o build/pc/time.o
+cc -DPLATFORM_PC -c src/platform/pc/dvdmgr_pc.c -o build/pc/dvdmgr_pc.o
+
+cc -DPLATFORM_PC \
+  pc_main.c \
+  build/pc/file.o \
+  build/pc/input.o \
+  build/pc/time.o \
+  build/pc/dvdmgr_pc.o \
+  -o build/pc/ttyd_pc
+
+./build/pc/ttyd_pc
