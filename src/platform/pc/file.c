975df8b982bb39464b0f87865112127fc937ea7e
nexus <122535095+nexuss2@users.noreply.github.com>
2026-05-04 13:48:45 +0000

    Implement PC file exists and open

--- src/platform/pc/file.c
+++ src/platform/pc/file.c
@@ -1,12 +1,49 @@
 #include <dolphin/dvd.h>
 #include <dolphin/types.h>
+#include <stdio.h>
+#include <string.h>
+
+#define PC_GAME_FILES_ROOT "orig/G8MJ01/files"
+
+static void make_pc_path(char* out, const char* path) {
+    if (path[0] == '/') {
+        sprintf(out, "%s%s", PC_GAME_FILES_ROOT, path);
+    } else {
+        sprintf(out, "%s/%s", PC_GAME_FILES_ROOT, path);
+    }
+}
 
 BOOL PlatformFileExists(const char* path) {
-    return FALSE;
+    FILE* f;
+    char pc_path[512];
+
+    make_pc_path(pc_path, path);
+    f = fopen(pc_path, "rb");
+    if (!f) {
+        return FALSE;
+    }
+
+    fclose(f);
+    return TRUE;
 }
 
 BOOL PlatformFileOpen(const char* path, DVDFileInfo* info) {
-    return FALSE;
+    FILE* f;
+    char pc_path[512];
+
+    make_pc_path(pc_path, path);
+    f = fopen(pc_path, "rb");
+    if (!f) {
+        return FALSE;
+    }
+
+    fseek(f, 0, SEEK_END);
+    info->length = (u32)ftell(f);
+    info->startAddr = 0;
+    info->callback = NULL;
+    fclose(f);
+
+    return TRUE;
 }
 
 s32 PlatformFileRead(DVDFileInfo* info, void* address, u32 size, s32 offset) {
