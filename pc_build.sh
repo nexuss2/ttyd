bd40646af8f3d9228716c655b664e25d9402fa00
nexus <122535095+nexuss2@users.noreply.github.com>
2026-05-04 17:43:26 +0000

    Build PC CMPR texture export test

--- pc_build.sh
+++ pc_build.sh
@@ -37,6 +37,7 @@ cc -DPLATFORM_PC pc_arcdrv_test.c build/pc/file.o build/pc/arcdrv_pc.o -o build/
 cc -DPLATFORM_PC pc_map_test.c build/pc/file.o -o build/pc/map_test
 cc -DPLATFORM_PC pc_tpl_test.c build/pc/file.o build/pc/arcdrv_pc.o build/pc/tpl_pc.o -o build/pc/tpl_test
 cc -DPLATFORM_PC pc_tpl_export_test.c build/pc/file.o build/pc/arcdrv_pc.o build/pc/tpl_export_pc.o -o build/pc/tpl_export_test
+cc -DPLATFORM_PC pc_tpl_cmpr_test.c build/pc/file.o build/pc/arcdrv_pc.o build/pc/tpl_export_pc.o -o build/pc/tpl_cmpr_test
 
 ./build/pc/ttyd_pc
 ./build/pc/asset_test
@@ -44,3 +45,4 @@ cc -DPLATFORM_PC pc_tpl_export_test.c build/pc/file.o build/pc/arcdrv_pc.o build
 ./build/pc/map_test
 ./build/pc/tpl_test
 ./build/pc/tpl_export_test
+./build/pc/tpl_cmpr_test
