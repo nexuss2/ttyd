83d205db545318e18b62b3f9dfaf00cdcd18821f
nexus <122535095+nexuss2@users.noreply.github.com>
2026-05-04 13:43:56 +0000

    Add PLATFORM_PC timing seam (main loop)

--- src/main.c
+++ src/main.c
@@ -3,6 +3,10 @@
 #include "mario/mariost.h"
 #include "sdk/DEMOInit.h"
 
+#ifdef PLATFORM_PC
+OSTick PlatformGetTick(void);
+#endif
+
 extern GlobalWork* gp;
 
 int main(void) {
@@ -11,13 +15,25 @@ int main(void) {
     marioStInit();
     marioStMain();
     while (1) {
+#ifdef PLATFORM_PC
+        base_tick = PlatformGetTick();
+#else
         base_tick = OSGetTick();
+#endif
         DEMOBeforeRender();
         marioStDisp();
         marioStMain();
+#ifdef PLATFORM_PC
+        gp->mDeltaGame = PlatformGetTick() - base_tick;
+#else
         gp->mDeltaGame = OSGetTick() - base_tick;
+#endif
         DEMODoneRender();
+#ifdef PLATFORM_PC
+        gp->mDeltaRender = PlatformGetTick() - base_tick;
+#else
         gp->mDeltaRender = OSGetTick() - base_tick;
+#endif
     }
 
     return 0;
