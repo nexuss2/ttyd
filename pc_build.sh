6bd4488425b908b35b4715bb400635f6d2d0f3bb
nexus <122535095+nexuss2@users.noreply.github.com>
2026-05-04 17:37:24 +0000

    Build PC TPL inspector test

--- pc_build.sh
+++ pc_build.sh
@@ -14,6 +14,7 @@ cc -DPLATFORM_PC -c src/platform/pc/envdrv_pc.c -o build/pc/envdrv_pc.o
 cc -DPLATFORM_PC -c src/platform/pc/windowdrv_pc.c -o build/pc/windowdrv_pc.o
 cc -DPLATFORM_PC -c src/platform/pc/effdrv_pc.c -o build/pc/effdrv_pc.o
 cc -DPLATFORM_PC -c src/platform/pc/mapdrv_pc.c -o build/pc/mapdrv_pc.o
+cc -DPLATFORM_PC -c src/platform/pc/tpl_pc.c -o build/pc/tpl_pc.o
 
 cc -DPLATFORM_PC \
   pc_main.c \
@@ -33,8 +34,10 @@ cc -DPLATFORM_PC \
 cc -DPLATFORM_PC pc_asset_test.c build/pc/file.o -o build/pc/asset_test
 cc -DPLATFORM_PC pc_arcdrv_test.c build/pc/file.o build/pc/arcdrv_pc.o -o build/pc/arcdrv_test
 cc -DPLATFORM_PC pc_map_test.c build/pc/file.o -o build/pc/map_test
+cc -DPLATFORM_PC pc_tpl_test.c build/pc/file.o build/pc/arcdrv_pc.o build/pc/tpl_pc.o -o build/pc/tpl_test
 
 ./build/pc/ttyd_pc
 ./build/pc/asset_test
 ./build/pc/arcdrv_test
 ./build/pc/map_test
+./build/pc/tpl_test
