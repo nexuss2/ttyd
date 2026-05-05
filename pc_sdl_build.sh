c3284b211202e066fc00d6661a0fc3e0e7ae87b3
nexus <122535095+nexuss2@users.noreply.github.com>
2026-05-05 17:51:25 +0000

    Build SDL scene file test

--- pc_sdl_build.sh
+++ pc_sdl_build.sh
@@ -84,6 +84,18 @@ cc -DPLATFORM_PC -Iinclude \
   $(pkg-config --libs sdl2)
 
 cc -DPLATFORM_PC -Iinclude \
+  pc_sdl_scene_file_test.c \
+  build/pc/file.o \
+  build/pc/arcdrv_pc.o \
+  build/pc/mapdrv_pc.o \
+  build/pc/tpl_export_pc.o \
+  build/pc/render_sdl_pc.o \
+  build/pc/texture_runtime_sdl.o \
+  build/pc/map_texture_runtime_sdl.o \
+  -o build/pc/sdl_scene_file_test \
+  $(pkg-config --libs sdl2)
+
+cc -DPLATFORM_PC -Iinclude \
   pc_sdl_fake_scene_test.c \
   build/pc/file.o \
   build/pc/arcdrv_pc.o \
@@ -117,3 +129,4 @@ cc -DPLATFORM_PC -Iinclude \
 ./build/pc/sdl_map_texture_set_test aaa_00
 ./build/pc/sdl_named_texture_test
 ./build/pc/sdl_fake_scene_test
+./build/pc/sdl_scene_file_test
