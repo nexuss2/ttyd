3d6d58b308960c1bad80f09f108cda9f51eba9c4
nexus <122535095+nexuss2@users.noreply.github.com>
2026-05-05 14:53:03 +0000

    Build PC message parser test

--- pc_build.sh
+++ pc_build.sh
@@ -14,6 +14,7 @@ cc -DPLATFORM_PC -c src/platform/pc/envdrv_pc.c -o build/pc/envdrv_pc.o
 cc -DPLATFORM_PC -c src/platform/pc/windowdrv_pc.c -o build/pc/windowdrv_pc.o
 cc -DPLATFORM_PC -c src/platform/pc/effdrv_pc.c -o build/pc/effdrv_pc.o
 cc -DPLATFORM_PC -c src/platform/pc/mapdrv_pc.c -o build/pc/mapdrv_pc.o
+cc -DPLATFORM_PC -c src/platform/pc/msgdrv_pc.c -o build/pc/msgdrv_pc.o
 cc -DPLATFORM_PC -c src/platform/pc/tpl_pc.c -o build/pc/tpl_pc.o
 cc -DPLATFORM_PC -c src/platform/pc/tpl_export_pc.c -o build/pc/tpl_export_pc.o
 
@@ -35,6 +36,7 @@ cc -DPLATFORM_PC \
 cc -DPLATFORM_PC pc_asset_test.c build/pc/file.o -o build/pc/asset_test
 cc -DPLATFORM_PC pc_arcdrv_test.c build/pc/file.o build/pc/arcdrv_pc.o -o build/pc/arcdrv_test
 cc -DPLATFORM_PC pc_map_test.c build/pc/file.o -o build/pc/map_test
+cc -DPLATFORM_PC pc_msg_test.c build/pc/file.o build/pc/msgdrv_pc.o -o build/pc/msg_test
 cc -DPLATFORM_PC pc_tpl_test.c build/pc/file.o build/pc/arcdrv_pc.o build/pc/tpl_pc.o -o build/pc/tpl_test
 cc -DPLATFORM_PC pc_tpl_export_test.c build/pc/file.o build/pc/arcdrv_pc.o build/pc/tpl_export_pc.o -o build/pc/tpl_export_test
 cc -DPLATFORM_PC pc_tpl_cmpr_test.c build/pc/file.o build/pc/arcdrv_pc.o build/pc/tpl_export_pc.o -o build/pc/tpl_cmpr_test
@@ -51,6 +53,7 @@ cc -DPLATFORM_PC pc_map_info_export.c build/pc/file.o build/pc/arcdrv_pc.o build
 ./build/pc/asset_test
 ./build/pc/arcdrv_test
 ./build/pc/map_test
+./build/pc/msg_test
 ./build/pc/tpl_test
 ./build/pc/tpl_export_test
 ./build/pc/tpl_cmpr_test
