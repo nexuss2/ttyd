845811d4e273c19d5cc94c9cfd93dffc880e080e
nexus <122535095+nexuss2@users.noreply.github.com>
2026-05-05 17:41:01 +0000

    Build SDL map texture set test

--- pc_sdl_build.sh
+++ pc_sdl_build.sh
@@ -6,6 +6,7 @@ mkdir -p build/pc
 cc -DPLATFORM_PC -Iinclude -c src/platform/pc/render_sdl_pc.c -o build/pc/render_sdl_pc.o $(pkg-config --cflags sdl2)
 cc -DPLATFORM_PC -Iinclude -c src/platform/pc/tpl_export_pc.c -o build/pc/tpl_export_pc.o
 cc -DPLATFORM_PC -Iinclude -c src/platform/pc/texture_runtime_sdl.c -o build/pc/texture_runtime_sdl.o
+cc -DPLATFORM_PC -Iinclude -c src/platform/pc/map_texture_runtime_sdl.c -o build/pc/map_texture_runtime_sdl.o
 cc -DPLATFORM_PC -c src/platform/pc/file.c -o build/pc/file.o
 cc -DPLATFORM_PC -c src/platform/pc/arcdrv_pc.c -o build/pc/arcdrv_pc.o
 
@@ -19,6 +20,17 @@ cc -DPLATFORM_PC -Iinclude \
   pc_sdl_rgb_test.c \
   build/pc/render_sdl_pc.o \
   -o build/pc/sdl_rgb_test \
+  $(pkg-config --libs sdl2)
+
+cc -DPLATFORM_PC -Iinclude \
+  pc_sdl_map_texture_set_test.c \
+  build/pc/file.o \
+  build/pc/arcdrv_pc.o \
+  build/pc/tpl_export_pc.o \
+  build/pc/render_sdl_pc.o \
+  build/pc/texture_runtime_sdl.o \
+  build/pc/map_texture_runtime_sdl.o \
+  -o build/pc/sdl_map_texture_set_test \
   $(pkg-config --libs sdl2)
 
 cc -DPLATFORM_PC -Iinclude \
@@ -75,3 +87,4 @@ cc -DPLATFORM_PC -Iinclude \
 ./build/pc/sdl_texture_cache_test
 ./build/pc/sdl_texture_runtime_test
 ./build/pc/sdl_runtime_texture_sheet_test
+./build/pc/sdl_map_texture_set_test
