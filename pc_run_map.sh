52b04e62e3fd7b632a1ec728ca99b86bda0f94d4
nexus <122535095+nexuss2@users.noreply.github.com>
2026-05-05 18:27:20 +0000

    Clean real map run helper

--- pc_run_map.sh
+++ pc_run_map.sh
@@ -3,7 +3,12 @@ set -e
 
 MAP="${1:-aaa_00}"
 MODE="${2:-auto}"
+EXTRA="${3:-}"
 
-./build/pc/sdl_real_map_runner "$MAP" "$MODE"
+if [ -n "$EXTRA" ]; then
+  ./build/pc/sdl_real_map_runner "$MAP" "$MODE" "$EXTRA"
+else
+  ./build/pc/sdl_real_map_runner "$MAP" "$MODE"
+fi
 
 echo "Wrote build/pc/sdl_real_map_runner.bmp"
