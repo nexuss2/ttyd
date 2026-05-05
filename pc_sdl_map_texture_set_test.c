41657ae8cfa746e14838ab96deea96e418ecc7a0
nexus <122535095+nexuss2@users.noreply.github.com>
2026-05-05 17:41:45 +0000

    Allow SDL map texture set test to choose map

--- pc_sdl_map_texture_set_test.c
+++ pc_sdl_map_texture_set_test.c
@@ -2,14 +2,22 @@
 #include "platform/pc/render_sdl_pc.h"
 #include "platform/pc/map_texture_runtime_sdl.h"
 
-int main(void) {
+int main(int argc, char** argv) {
+    const char* map = "aaa_00";
+    char out_path[256];
     PCMapTextureSet* set;
+
+    if (argc > 1) {
+        map = argv[1];
+    }
+
+    sprintf(out_path, "build/pc/sdl_%s_texture_set_test.bmp", map);
 
     if (!PCRenderSDLInit(960, 760)) {
         return 1;
     }
 
-    set = PCMapTextureSetLoad("aaa_00");
+    set = PCMapTextureSetLoad(map);
     if (!set) {
         PCRenderSDLShutdown();
         return 1;
@@ -20,10 +28,11 @@ int main(void) {
     PCMapTextureSetDrawSheet(set, 32, 32);
     PCRenderSDLEndFrame();
 
-    PCRenderSDLSaveBMP("build/pc/sdl_map_texture_set_test.bmp");
+    PCRenderSDLSaveBMP(out_path);
 
     PCMapTextureSetDestroy(set);
     PCRenderSDLShutdown();
 
+    printf("wrote %s\n", out_path);
     return 0;
 }
