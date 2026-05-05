fbcbae6964254d187e416bd5b8142b1c188a402a
nexus <122535095+nexuss2@users.noreply.github.com>
2026-05-05 17:46:33 +0000

    Clean up SDL build script

--- pc_sdl_build.sh
+++ pc_sdl_build.sh
@@ -3,12 +3,14 @@ set -e
 
 mkdir -p build/pc
 
+cc -DPLATFORM_PC -c src/platform/pc/file.c -o build/pc/file.o
+cc -DPLATFORM_PC -c src/platform/pc/arcdrv_pc.c -o build/pc/arcdrv_pc.o
+cc -DPLATFORM_PC -c src/platform/pc/mapdrv_pc.c -o build/pc/mapdrv_pc.o
+
 cc -DPLATFORM_PC -Iinclude -c src/platform/pc/render_sdl_pc.c -o build/pc/render_sdl_pc.o $(pkg-config --cflags sdl2)
 cc -DPLATFORM_PC -Iinclude -c src/platform/pc/tpl_export_pc.c -o build/pc/tpl_export_pc.o
 cc -DPLATFORM_PC -Iinclude -c src/platform/pc/texture_runtime_sdl.c -o build/pc/texture_runtime_sdl.o
 cc -DPLATFORM_PC -Iinclude -c src/platform/pc/map_texture_runtime_sdl.c -o build/pc/map_texture_runtime_sdl.o
-cc -DPLATFORM_PC -c src/platform/pc/file.c -o build/pc/file.o
-cc -DPLATFORM_PC -c src/platform/pc/arcdrv_pc.c -o build/pc/arcdrv_pc.o
 
 cc -DPLATFORM_PC -Iinclude \
   pc_sdl_window_test.c \
@@ -23,24 +25,30 @@ cc -DPLATFORM_PC -Iinclude \
   $(pkg-config --libs sdl2)
 
 cc -DPLATFORM_PC -Iinclude \
-  pc_sdl_map_texture_set_test.c \
+  pc_sdl_cmpr_direct_test.c \
   build/pc/file.o \
   build/pc/arcdrv_pc.o \
   build/pc/tpl_export_pc.o \
   build/pc/render_sdl_pc.o \
-  build/pc/texture_runtime_sdl.o \
-  build/pc/map_texture_runtime_sdl.o \
-  -o build/pc/sdl_map_texture_set_test \
+  -o build/pc/sdl_cmpr_direct_test \
   $(pkg-config --libs sdl2)
 
 cc -DPLATFORM_PC -Iinclude \
-  pc_sdl_runtime_texture_sheet_test.c \
+  pc_sdl_cmpr_sheet_test.c \
   build/pc/file.o \
   build/pc/arcdrv_pc.o \
   build/pc/tpl_export_pc.o \
   build/pc/render_sdl_pc.o \
-  build/pc/texture_runtime_sdl.o \
-  -o build/pc/sdl_runtime_texture_sheet_test \
+  -o build/pc/sdl_cmpr_sheet_test \
+  $(pkg-config --libs sdl2)
+
+cc -DPLATFORM_PC -Iinclude \
+  pc_sdl_texture_cache_test.c \
+  build/pc/file.o \
+  build/pc/arcdrv_pc.o \
+  build/pc/tpl_export_pc.o \
+  build/pc/render_sdl_pc.o \
+  -o build/pc/sdl_texture_cache_test \
   $(pkg-config --libs sdl2)
 
 cc -DPLATFORM_PC -Iinclude \
@@ -54,30 +62,37 @@ cc -DPLATFORM_PC -Iinclude \
   $(pkg-config --libs sdl2)
 
 cc -DPLATFORM_PC -Iinclude \
-  pc_sdl_texture_cache_test.c \
+  pc_sdl_runtime_texture_sheet_test.c \
   build/pc/file.o \
   build/pc/arcdrv_pc.o \
   build/pc/tpl_export_pc.o \
   build/pc/render_sdl_pc.o \
-  -o build/pc/sdl_texture_cache_test \
+  build/pc/texture_runtime_sdl.o \
+  -o build/pc/sdl_runtime_texture_sheet_test \
   $(pkg-config --libs sdl2)
 
 cc -DPLATFORM_PC -Iinclude \
-  pc_sdl_cmpr_sheet_test.c \
+  pc_sdl_map_texture_set_test.c \
   build/pc/file.o \
   build/pc/arcdrv_pc.o \
+  build/pc/mapdrv_pc.o \
   build/pc/tpl_export_pc.o \
   build/pc/render_sdl_pc.o \
-  -o build/pc/sdl_cmpr_sheet_test \
+  build/pc/texture_runtime_sdl.o \
+  build/pc/map_texture_runtime_sdl.o \
+  -o build/pc/sdl_map_texture_set_test \
   $(pkg-config --libs sdl2)
 
 cc -DPLATFORM_PC -Iinclude \
-  pc_sdl_cmpr_direct_test.c \
+  pc_sdl_named_texture_test.c \
   build/pc/file.o \
   build/pc/arcdrv_pc.o \
+  build/pc/mapdrv_pc.o \
   build/pc/tpl_export_pc.o \
   build/pc/render_sdl_pc.o \
-  -o build/pc/sdl_cmpr_direct_test \
+  build/pc/texture_runtime_sdl.o \
+  build/pc/map_texture_runtime_sdl.o \
+  -o build/pc/sdl_named_texture_test \
   $(pkg-config --libs sdl2)
 
 ./build/pc/sdl_window_test
@@ -88,3 +103,4 @@ cc -DPLATFORM_PC -Iinclude \
 ./build/pc/sdl_texture_runtime_test
 ./build/pc/sdl_runtime_texture_sheet_test
 ./build/pc/sdl_map_texture_set_test aaa_00
+./build/pc/sdl_named_texture_test
