f09df67a8995701e287d83a86131d54bc833a2d1
nexus <122535095+nexuss2@users.noreply.github.com>
2026-05-05 07:03:49 +0000

    Build PC I8 texture export test

--- pc_build.sh
+++ pc_build.sh
@@ -40,6 +40,7 @@ cc -DPLATFORM_PC pc_tpl_export_test.c build/pc/file.o build/pc/arcdrv_pc.o build
 cc -DPLATFORM_PC pc_tpl_cmpr_test.c build/pc/file.o build/pc/arcdrv_pc.o build/pc/tpl_export_pc.o -o build/pc/tpl_cmpr_test
 cc -DPLATFORM_PC pc_tpl_export_all_test.c build/pc/file.o build/pc/arcdrv_pc.o build/pc/tpl_export_pc.o -o build/pc/tpl_export_all_test
 cc -DPLATFORM_PC pc_tpl_rgb565_test.c build/pc/file.o build/pc/arcdrv_pc.o build/pc/tpl_export_pc.o -o build/pc/tpl_rgb565_test
+cc -DPLATFORM_PC pc_tpl_i8_test.c build/pc/file.o build/pc/arcdrv_pc.o build/pc/tpl_export_pc.o -o build/pc/tpl_i8_test
 
 ./build/pc/ttyd_pc
 ./build/pc/asset_test
@@ -50,3 +51,4 @@ cc -DPLATFORM_PC pc_tpl_rgb565_test.c build/pc/file.o build/pc/arcdrv_pc.o build
 ./build/pc/tpl_cmpr_test
 ./build/pc/tpl_export_all_test
 ./build/pc/tpl_rgb565_test
+./build/pc/tpl_i8_test
